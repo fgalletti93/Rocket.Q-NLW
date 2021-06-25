e02b60a84bb984e9267cbfd7480b8e95ccec9ab3
Francesca Beatrice Galletti <fgalletti93@yahoo.com>
2021-06-25 14:20:19 -0300

    continuação de aula 04 - trabalhando com JavaScript e Node

--- src/route.js
+++ src/route.js
@@ -3,5 +3,10 @@ const express = require('express')
 const route = express.Router()
 
 route.get('/', (req, res) => res.render("index"))
+route.get('/room', (req, res) => res.render("room"))
+route.get('/create-room', (req, res) => res.render("create-room"))
+
+//Formato que o formulário de dentro da modal tem que passar a infor
+//route.post('/room/:room/:question/:action')
 
 module.exports = route
