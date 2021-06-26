3d8329b872e26508fde155ddfd88c9ec241d697e
Francesca Beatrice Galletti <fgalletti93@yahoo.com>
2021-06-26 15:40:16 -0300

    problemas com banco de dados

--- src/db/config.js
+++ src/db/config.js
@@ -3,6 +3,6 @@ const { open } = require("sqlite")
 
 module.exports = () => 
     open({
-        filename: '/src/db/rocketq.sqlite',
-        driver: sqlite3.Database, 
-    })
+        filename: './src/db/rocketq.sqlite',
+        driver: sqlite3.Database,
+    });
--- src/db/init.js
+++ src/db/init.js
@@ -4,7 +4,19 @@ const initDb = {
     async init(){
         const db = await Database()
 
-        await db.exec()
+        await db.exec(`CREATE TABLE rooms (
+            id INTEGER PRIMARY KEY,
+            pass TEXT
+        )`);
+
+        await db.exec(`CREATE TABLE questions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT,
+            read INT,
+            room INT
+        )`);
+
+        await db.close()
     }
 }
 
