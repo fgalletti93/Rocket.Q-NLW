4bcd0392c5b2504d7184c7a21212d296c38fd289
Francesca Beatrice Galletti <fgalletti93@yahoo.com>
2021-06-28 12:25:14 -0300

    marcando como lida e excluindo perguntas

--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -2,13 +2,27 @@ const Database = require('../db/config')
 
 module.exports ={
 
-    index(req, res){
+    async index(req, res){
+        const db = await Database()
         const roomId = req.params.room;
         const questionId = req.params.question;
         const action = req.params.action;
         const password = req.body.password
+        
+        /*  Verificar se a senha está correta*/
+        const verifyRoom = await db.get(`SELECT * FROM rooms WHERE id = ${roomId}`)
+        if(verifyRoom.pass == password){
+            if(action == "delete"){
 
-        console.log(`room = ${roomId}, questionId = ${questionId}, action = ${action}, password = ${password}`)
+                await db.run(`DELETE FROM questions WHERE id = ${questionId}`)
+
+            }else if(action == "check"){
+
+                await db.run(`UPDATE questions SET read = 1 WHERE id = ${questionId}`)
+            }
+        }
+        res.redirect(`/room/${roomId}`)
+
     },
 
     async create(req, res){
